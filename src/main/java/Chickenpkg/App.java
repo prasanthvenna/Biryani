2e8e388e8fc70d41f77943ae50c3695bfd60106e
prasanthvenna <115454218+prasanthvenna@users.noreply.github.com>
2022-10-25 21:09:45 +0530

    Add files via upload

---
+++ src/main/java/Chickenpkg/App.java
@@ -0,0 +1,13 @@
+package Chickenpkg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
